c260b43eadb32e6f2740545122ee021dd0931f4f
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-06-05 14:41:13 +0300

    intergrate and test PayPal subscribe button

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -15,6 +15,22 @@ class Profile extends React.Component {
   async componentDidMount() {
     await this.props.setCurrentUser();
   }
+
+  paypalSubscribe = (data, actions) => {
+    return actions.subscription.create({
+      'plan_id': "P-3H226719GE121945VMC5IQTI",
+    });
+  };test
+
+  paypalOnError = (err) => {
+      alert('Error')
+  }
+
+  paypalOnApprove = (data, detail) => {
+     console.log(data)
+     alert('Success')
+  };
+
   render() {
     if (this.props.auth.isAuthenticated) {
       return (
@@ -28,17 +44,36 @@ class Profile extends React.Component {
               <li>Your Email: {this.props.auth.user.email}</li>
               <img className="photo" src={this.props.auth.user.photo} alt="" />
             </ul>
+              {/*<PayPalButton*/}
+              {/*    amount="10.0"*/}
+              {/*    onSuccess={(details, data) => {*/}
+              {/*        alert("Transaction completed");*/}
+              {/*        // OPTIONAL: Call your server to save the transaction*/}
+              {/*        return fetch("/paypal-transaction-complete", {*/}
+              {/*            method: "post",*/}
+              {/*            body: JSON.stringify({*/}
+              {/*                orderID: data.orderID*/}
+              {/*            })*/}
+              {/*        });*/}
+              {/*    }}*/}
+              {/*/>*/}
               <PayPalButton
-                  amount="0.01"
-                  onSuccess={(details, data) => {
-                      alert("Transaction completed");
-                      // OPTIONAL: Call your server to save the transaction
-                      return fetch("/paypal-transaction-complete", {
-                          method: "post",
-                          body: JSON.stringify({
-                              orderID: data.orderID
-                          })
-                      });
+                  amount="20.0"
+                  currency="USD"
+                  createSubscription={(data, details) => this.paypalSubscribe(data, details)}
+                  onApprove={(data, details) => this.paypalOnApprove(data, details)}
+                  options={{
+                      clientId: 'AZRtameGwLo6f_zKc73fnXRoR8zZX-dFzlHci18FIXRUlMY2rtdpZVPnXyYx5QhMDcZ0sE9tjDuDKSRR',
+                      vault:true
+                  }}
+                  onError={(err) => this.paypalOnError(err)}
+                  catchError={(err) => this.paypalOnError(err)}
+                  onCancel={(err) => this.paypalOnError(err)}
+                  style={{
+                      shape: 'rect',
+                      color: 'blue',
+                      layout: 'vertical',
+                      label: 'subscribe',
                   }}
               />
           </center>
