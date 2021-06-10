7f920c634739267ccf26aec2412736dc4573d851
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-06-10 23:37:13 +0300

    use private paths from PrivatePath, remove use library

---
+++ client/src/routers/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+export const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isAuthenticated ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: '/',
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+            }
+        />
+    )
+}
